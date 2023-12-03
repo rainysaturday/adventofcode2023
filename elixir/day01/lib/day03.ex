fe8437a5e8f8dee33c6d7c912285abfcc6076c34
rainysaturday <rainysaturday@github.com>
2023-12-03 10:51:42 +0100

    Day03 part 1
    Signed-off-by: rainysaturday <rainysaturday@github.com>

---
+++ elixir/day01/lib/day03.ex
@@ -0,0 +1,162 @@
+
+defmodule Day03 do
+  @doc """
+    iex> Day03.part1
+    539637
+  """
+  def part1 do
+    lines = Helpers.read_input_by_lines("../../inputs/day03.txt")
+    world = lines_to_map(lines)
+
+    #traverse world
+    parts = lines
+    |> Enum.with_index(1)
+    |> Enum.map(fn {l, y} -> {l, y - 1} end) # with_index starts at 1 :(
+    |> Enum.reduce(%{}, fn {l, y}, acc ->
+      0..String.length(l)
+      |> Enum.reduce(acc, fn x, acc ->
+        case get_num_from_map(world, x, y) do
+          nil -> acc
+          val ->
+            if has_symbol_adjacent(world, x, y) do
+              start = get_start_of_num(world[y], x)
+              Map.put(acc, {start, y}, val)
+            else
+              acc
+            end
+        end
+      end)
+    end)
+
+    Map.values(parts)
+    |> Enum.sum()
+  end
+
+  @doc """
+    iex> Day03.part2
+    1
+  """
+  def part2 do
+    Helpers.read_input_by_lines("../../inputs/day03.txt")
+    1
+  end
+
+   @doc """
+    iex> Day03.has_symbol_adjacent(%{0 => %{0 => ".", 1 => "."}, 1 => %{0 => "1", 1 => "2"}}, 1, 1)
+    false
+    iex> Day03.has_symbol_adjacent(%{0 => %{0 => "=", 1 => "."}, 1 => %{0 => "1", 1 => "2"}}, 1, 1)
+    true
+  """
+  def has_symbol_adjacent(m, x, y) do
+    r = [-1, 0, 1]
+    Enum.any?(r, fn dx ->
+      Enum.any?(r, fn dy ->
+        is_symbol(m, x + dx, y + dy)
+       end)
+    end)
+  end
+
+  @doc """
+    iex> Day03.is_symbol(%{0 => %{0 => "=", 1 => "."}, 1 => %{0 => "1", 1 => "2"}}, 1, 1)
+    false
+    iex> Day03.is_symbol(%{0 => %{0 => "=", 1 => "."}, 1 => %{0 => "1", 1 => "2"}}, 0, 0)
+    true
+  """
+  def is_symbol(m, x, y) do
+    case m[y] do
+      nil -> false
+      line -> case line[x] do
+        nil -> false
+        "." -> false
+        _other -> case get_num_from_map(m, x, y) do
+          nil -> true # The current position is not outside, and also not a dot or a number, so must be a symbol
+          _isnum -> false
+        end
+      end
+    end
+  end
+
+
+
+  @doc """
+    iex> Day03.get_num_from_map(%{0 => %{0 => ".", 1 => "."}, 1 => %{0 => "1", 1 => "2"}}, 1, 1)
+    12
+  """
+  def get_num_from_map(m, x, y) do
+    case m[y] do
+      nil -> nil
+      l -> case get_num_from_line(l, x, 0) do
+        "" -> nil
+        num -> {val, _rest} = Integer.parse(num)
+          val
+      end
+    end
+  end
+
+  @doc """
+    iex> Day03.get_num_from_line(%{0 => "1", 1 => "2"}, 1, 0)
+    "12"
+  """
+  def get_num_from_line(l, x, dir) do
+    case l[x] do
+      nil -> ""
+      c ->
+        case {Integer.parse(c), dir} do
+          {:error, _} -> ""
+          {{_val, _rest}, 0} -> get_num_from_line(l, x - 1, -1) <> c <> get_num_from_line(l, x + 1, 1)
+          {{_val, _rest}, -1} -> get_num_from_line(l, x - 1, dir) <> c
+          {{_val, _rest}, 1} -> c <> get_num_from_line(l, x + 1, dir)
+        end
+    end
+  end
+
+  @doc """
+    iex> Day03.get_start_of_num(%{0 => "1", 1 => "2"}, 1)
+    0
+  """
+  def get_start_of_num(l, x) do
+    case l[x] do
+      nil -> nil
+      c ->
+        case Integer.parse(c) do
+          :error -> nil
+          {_val, _rest} -> case get_start_of_num(l, x - 1) do
+            nil -> x
+            otherwise -> otherwise
+          end
+        end
+    end
+  end
+
+  @doc """
+    iex> Day03.lines_to_map(["..", "--"])
+    %{0 => %{0 => ".", 1 => "."}, 1 => %{0 => "-", 1 => "-"}}
+  """
+  def lines_to_map(lines) do
+    lines
+    |> Enum.with_index(1)
+    |> Enum.reduce(%{}, fn {l, i}, acc ->
+      l = line_to_map(l)
+      Map.put(acc, i - 1, l)
+    end)
+  end
+
+  @doc """
+    iex> Day03.line_to_map("..1=")
+    %{0 => ".", 1 => ".", 2 => "1", 3 => "="}
+  """
+  #spec line_to_map(str :: charlist()) :: map()
+  def line_to_map(str) do
+    String.split(str, "")
+    |> Enum.filter(fn s -> String.length(s) > 0 end)
+    |> Enum.with_index(1)
+    |> Enum.map(fn {s, i} -> {i - 1, s} end)
+    |> Enum.reduce(%{}, fn val, acc ->
+      {key, val} = val
+      Map.put(acc, key, val)
+    end)
+  end
+
+
+
+end
--- elixir/day01/test/day1_test.exs
+++ elixir/day01/test/day1_test.exs
@@ -1,7 +1,8 @@
 defmodule Day1Test do
   use ExUnit.Case
-  doctest Day1
-  doctest Day02
+  #doctest Day1
+  #doctest Day02
+  doctest Day03
 
   test "greets the world" do
   end
